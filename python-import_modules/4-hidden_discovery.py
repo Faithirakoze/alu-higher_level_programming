2f6f4fc48214af5e709499e86b179b87633de95e
Faithirakoze <f.irakoze2@alustudent.com>
2025-03-07 14:54:59 -0800

    Task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
+
 import hidden_4
 
 
 if __name__ == "__main__":
-    content = dir(hidden_4)
-    count = len(content)
-    names = [name for name in content if not name.startswith("__")]
-    for i sorted(names):
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+
+    for i in sorted(names):
         print(i)
