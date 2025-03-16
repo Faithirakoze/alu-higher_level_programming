e4ab303985f812846a7b63734a7801389f56582b
Faithirakoze <f.irakoze2@alustudent.com>
2025-03-15 23:28:39 -0700

    Task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,11 +3,11 @@
 
 
 class Square:
-    """Initializing an object with an attribute"""
+    """a class that initialiazes an object with an attribute"""
     def __init__(self, size=0):
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
