ca9ea23c4daf90717a848fa76c0237f921bfecb1
Faithirakoze <f.irakoze2@alustudent.com>
2025-03-23 19:17:46 -0700

    Task 3

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+"""Creating a class that defines a rectangle by:
+ width, height, area, and perimeter.
+ Representing a rectangle by string of # characters"""
+
+
+class Rectangle:
+    """Private instance attributes(width and height)
+    Use of property getters and setters
+    Public instance methods for area and perimeter
+    Use of __str__() to return the rectangle with the # character
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        result = 2 * (self.__width + self.__height)
+        return result
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        result = ""
+        for i in range(self.__height):
+            result += "#" * self.__width + "\n"
+            return result.rstrip()
