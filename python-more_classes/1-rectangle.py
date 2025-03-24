0cfdda61d751a4ac7926e473bfb642b59c52d595
Faithirakoze <f.irakoze2@alustudent.com>
2025-03-23 18:36:11 -0700

    Task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""Creating a class that defines a rectangle by width and height"""
+
+
+class Rectangle:
+    """Private instance attributes(width and height)
+    Use of property getters and setters
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
