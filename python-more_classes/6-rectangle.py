e0ca624007fcc674dc973060c8b6f661892ba833
Faithirakoze <f.irakoze2@alustudent.com>
2025-03-23 21:40:04 -0700

    Task 6

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+"""Creating a class that defines a rectangle by:
+ width, height, area, and perimeter.
+ Representing a rectangle by string of # characters"""
+
+
+class Rectangle:
+    """Private instance attributes(width and height)"""
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        result = 2 * (self.__width + self.__height)
+        return result
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle using '#' characters.
+
+        If width or height is 0, it returns an empty string.
+        """
+        if self.width == 0 or self.height == 0:
+            return ""
+        return "\n".join("#" * self.width for _ in range(self.height))
+
+    def __repr__(self):
+        """Returns a string that recreates the rectangle instance"""
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        """Prints a message when an instance is deleted"""
+        print("Bye rectangle...")
