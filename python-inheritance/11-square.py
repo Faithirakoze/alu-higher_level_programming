3fc1925649acecb26a4dce4f318b617a3da94d5c
Faithirakoze <f.irakoze2@alustudent.com>
2025-04-28 20:29:52 -0700

    Task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""this module defines a Rectangle subclass Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square"""
+
+    def __init__(self, size):
+        """Initialize a new square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """returns the print() and str() representation of a Square"""
+        string = "[" + str(self.__class__.__name__)
