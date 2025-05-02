967dc6a5dfe86813509d48216bd43beb744fe62a
Faithirakoze <f.irakoze2@alustudent.com>
2025-05-02 10:01:10 -0700

    Task 4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- This script creates a new table called first_table in the current database.
+-- The table has two columns: id and name.
+
+-- This command will create a new table named first_table
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
