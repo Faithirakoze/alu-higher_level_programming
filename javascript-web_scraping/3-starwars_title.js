8a3281e211c43e3c7bb8cde05e535c7ae3fb5911
Faithirakoze <f.irakoze2@alustudent.com>
2025-06-07 10:26:05 -0700

    Task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/:id' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+
+const movieId = process.argv[2];
+if (!movieId) {
+  console.log('Usage: ./starwars_title.js <movie_id>');
+  process.exit(1);
+}
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Error: Status code ${response.statusCode}`);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
 });
